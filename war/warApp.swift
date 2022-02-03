4751dd741bd2a6d170b65607f8246486f8ccad4f
90306561 <72754329+90306561@users.noreply.github.com>
2022-02-03 10:46:02 -0600

    Initial Commit

---
+++ war/warApp.swift
@@ -0,0 +1,17 @@
+//
+//  warApp.swift
+//  war
+//
+//  Created by 90306561 on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct warApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
